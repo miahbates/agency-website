5466f74d8a2cf8c8108fd61d7863bcc7c06c5b8a
Orian <77785778+OrianP@users.noreply.github.com>
2021-10-25 19:18:21 +0100

    cta remove cta event listener and instead add it to the 'tabs' nodelist to be included in for loop

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // target attribute 'data tab target' to get an array of li's 
-const tabs = document.querySelectorAll('.tabs[data-tab-target]');
+const tabs = document.querySelectorAll('[data-tab-target]');
 
 // target attribute 'data tab content' on contents of tabs
 const tabContents = document.querySelectorAll('[data-tab-content]'); 
-
-const cta = document.querySelector('.cta[data-tab-target]');
 
 // loop over tabs array to get each individual li 
 // add event listener for when clicked
@@ -25,14 +23,3 @@ for (let i = 0; i < tabs.length; i++) {
     //  showTab.add('active');
   })
 }
-
-// add event listener to call-to-action
-// hide the current active tab
-// reveal the tab that the cta is targeting in it's data-tab-target attribute
-cta.addEventListener('click', () => {
-  const activeTab = document.querySelector('section.active');
-  activeTab.classList.remove('active');
-  
-  const showTab = document.querySelector(cta.getAttribute('data-tab-target'));
-  showTab.classList.add('active');
-})
