d3d3a20dfc1219ded3c7b05fb760ca604c1ff940
Orian <77785778+OrianP@users.noreply.github.com>
2021-10-26 13:11:33 +0100

    form validate user inputs. display submit message with consultation date and time. submit form data. fixes #5

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ for (let i = 0; i < tabs.length; i++) {
     // const showTab = document.querySelector(tabs[i].getAttribute('data-tab-target'));
     // showTab.classList.add('active');
      const target = document.querySelector(tabs[i].dataset.tabTarget);
-     console.log(tabs[i].dataset.tabTarget, target);
+    //  console.log(tabs[i].dataset.tabTarget, target);
      target.dataset.active = true;
     //  showTab.add('active');
   })
@@ -26,21 +26,64 @@ for (let i = 0; i < tabs.length; i++) {
 
 // contact form
 const form = document.querySelector('form');
-// submit input
-const submitBtn = document.querySelector('.submit-btn');
-
 // submit message
 const submitMsg = document.querySelector('#submit-msg');
+// contact button
+const contactBtn = document.querySelector('[data-tab-target="#contact"]');
 
-console.log(form);
-console.log(submitBtn);
-console.log(submitMsg);
-
-// function to get all form inputs and return true if they are not empty 
-// call this function in the event listener before hiding the form 
-// function to get the date and time input values and display them in the submit message
-
-submitBtn.addEventListener('click', () => {
+form.addEventListener('submit', (e) => {
+  // prevent default submit behaviour of clearing inputs and displaying empty form 
+  e.preventDefault();
+  // save the form to submit later
+  let form = e.target;
+  validateFormVals();
+  displaySubmitMsg();
+  // reveal submit message
   submitMsg.dataset.active = true;
+  // hide form
   form.setAttribute('data-tab-content', "");
+  // show form if 'contact' button is clicked again after submission
+  contactBtn.addEventListener('click', () => {
+    form.removeAttribute('data-tab-content');
+  })
+  // submit form 
+  if (submitMsg.dataset.active){
+    form.submit();
+  }
+  // reset form inputs 
+  form.reset();
 })
+
+// get all form inputs and return true if they are valid 
+function validateFormVals() {
+  const inputs = document.querySelectorAll('input');
+  let isValid;
+  for (let val of inputs) {
+    if (val.type !== 'submit') {
+      isValid = val.validity.valid; 
+    }
+  }
+  console.log({isValid});
+  return isValid;
+}
+
+// get the date and time input values and display them in the submit message
+function displaySubmitMsg() {
+  // get input values
+  const name = document.querySelector('input[id = "first-name"]').value;
+  const dateObj = document.querySelector('input[type = "date"]').valueAsDate;
+  const select = document.querySelector('select');
+  const time = select.options[select.selectedIndex].value;
+
+  // update text submit message text content
+  let thanks = document.querySelector('.tnx');
+  thanks.textContent += `, ${name}!`;
+  let consultationInfo = document.querySelector('.date-time-format');
+  consultationInfo.textContent += `${formatDateTime(dateObj, time)}`;
+}
+
+// return a date and time formatted string i.e 'Tuesday 26 October 2021 at 10:00'
+function formatDateTime(dateObj, time) {
+  const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  return `${dateObj.toLocaleDateString('en-GB', options)} at ${time}`;
+}
